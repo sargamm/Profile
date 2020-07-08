4732a404572c6b580828109e302647beaf349f37
sargamm <sargam.monga@gmail.com>
2020-07-08 13:11:32 +0530

    Updates

---
+++ precache-manifest.e10b795d4b69f6b35c616f14e7ca6928.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7ceed13836d88b623700be27309d9d",
+    "url": "/Profile/index.html"
+  },
+  {
+    "revision": "09550178f62176a0fc9c",
+    "url": "/Profile/static/css/2.e53f431d.chunk.css"
+  },
+  {
+    "revision": "4f4a26b0670e5bfdacb0",
+    "url": "/Profile/static/css/main.fc422507.chunk.css"
+  },
+  {
+    "revision": "09550178f62176a0fc9c",
+    "url": "/Profile/static/js/2.aac6e1fe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Profile/static/js/2.aac6e1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4a26b0670e5bfdacb0",
+    "url": "/Profile/static/js/main.0dada412.chunk.js"
+  },
+  {
+    "revision": "aa583ed9812b2c07b603",
+    "url": "/Profile/static/js/runtime-main.2a7c0732.js"
+  },
+  {
+    "revision": "faf44cade0b211c8e36a4a2f43c5765d",
+    "url": "/Profile/static/media/bgachievements.faf44cad.png"
+  }
+]);
